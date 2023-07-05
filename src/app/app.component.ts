489e2bcccc0835c16ef1c8762028361075e3b21b
David McGrath <david.mcgrath13@gmail.com>
2023-07-05 10:01:11 +0800

    More experiments
    Still having issues, trying to track it down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,14 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 export class AppComponent {
   visualViewportHeight: string | null = null;
   constructor() {
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener('resize', (event) => {
+        this.resize();
+      });
+      this.resize();
+    }
+  }
+  private resize() {
     // TODO: Also check if on mobile, mobile interface will be slightly different
     // Need a service that handles this & is mobile, clean it up
     // On mobile show formatting toolbar above soft keyboard, on desktop show at top
@@ -20,21 +28,16 @@ export class AppComponent {
     // Need to fix... ensure the total height is reduced.
 
     // TODO: If the visual viewport matches the layout viewport, don't set the height (will default to 100% based on CSS)
+    let height = '';
     if (window.visualViewport) {
-      window.visualViewport.addEventListener('resize', (event) => {
-        const height = window.visualViewport?.height;
-        if (height) {
-          const heightPx = `${height}px`;
-          this.visualViewportHeight = heightPx;
-          document.body.style.height = heightPx;
-          document.documentElement.style.height = heightPx;
-        }
-        else {
-          this.visualViewportHeight = null;
-          document.body.style.height = '';
-          document.documentElement.style.height = '';
-        }
-      });
+      const visualHeight = window['testVisualViewport'] ? window.visualViewport.height / 2 : window.visualViewport.height;
+      const delta = visualHeight - window.innerHeight;
+      if (Math.abs(delta) >= 1 && visualHeight >= 1) {
+        height = `${visualHeight}px`;
+      }
     }
+    //this.visualViewportHeight = heightPx;
+    //document.body.style.height = heightPx;
+    document.documentElement.style.height = height;
   }
 }
